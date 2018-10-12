b447d5f50a000ed5a786cbc1efaa6d20813de49e
nir.segev <nir.segev@ladbrokescoral.com>
2018-10-12 14:07:18 +0300

    toggle displays

--- js/main.js
+++ js/main.js
@@ -3,34 +3,36 @@
 const model = {
     currentTrackId: '',
     batchDisplay: 6,
+    renderedTracksBatch: 0,
     lastSearchedStrings: [],
     currentResults: [],
     getTracks: (par) => {
-        SC.get('/tracks', {
-            limit: model.batchDisplay,
+        return SC.get('/tracks', {
+            //limit: model.batchDisplay,
             q: par
         }).then(function (tracks) {
             model.currentResults = tracks;
             console.log(model.currentResults);
-            view.printCurrentResults(model.currentResults);
+            // view.printCurrentResults(model.currentResults);
+            model.lastSearchedStrings.push(par);
+            console.log(model.lastSearchedStrings);
+            return Promise.resolve(tracks);
         });
     },
 };
 
+
+
 const view = {
+    dataDisplay:  document.querySelector("#data_display"),
+
     printCurrentResults: (tracks) => {
-        let dataDisplay = document.querySelector("#data_display");
-        console.log(tracks);
-        tracks.forEach((layer,index,array) => {
-            let track = document.createElement('li');
-            let trackLink = document.createElement('a');
-            track.classList.add('data-display__result');
-            trackLink.classList.add('data-display__link');
-            trackLink.setAttribute('track-id',layer.id);
-            trackLink.innerText = layer.title;
-            track.appendChild(trackLink);
-            dataDisplay.appendChild(track);
-        });
+        console.table(tracks);
+        view.dataDisplay.innerHTML = tracks
+            .map(t =>
+                `<li class="data-display__result" track-id="${t.id}">
+                    <a class="data-display__link" href="${t.id}">${t.title}</a>
+                </li>`).join('');
     }
 };
 
@@ -38,20 +40,36 @@ const controller = {
 
     searchGetInput: document.querySelector("#search_get_input"),
     searchSubmitter: document.querySelector('#search_submitter'),
-    user_id: 'EBquMMXE2x5ZxNs9UElOfb4HbvZK95rc',
+
+    // the elements with 'data-tab-id' attribute
+    tab_lis: document.querySelectorAll('[data-tab-id]'),
+
+    user_id: 'E8IqLGTYxHll6SyaM7LKrMzKveWkcrjg',
     init: () => {
         SC.initialize({client_id: controller.user_id});
         // console.log(controller);
 
+        // the elements with corresponding ids
+        const tabs = Array.from(controller.tab_lis)
+            .map(t=>document.getElementById(t.getAttribute('data-tab-id')));
+
+        controller.tab_lis.forEach((li, i)=> {
+            li.addEventListener('click', ()=> {
+                tabs.forEach(t => t.classList.remove('displayed'));
+                tabs[i].classList.add('displayed');
+            })
+        });
+
         // set event handlers
-        controller.searchSubmitter.onclick = controller.commitSearch;
-        controller.searchSubmitter.onclick = controller.commitSearch;
+        //controller.searchSubmitter.onclick = controller.commitSearch;
+        controller.searchSubmitter.addEventListener('click', controller.commitSearch);
+        controller.searchGetInput.addEventListener('change', controller.commitSearch);
     },
     commitSearch:
         () => {
             let searchValue = controller.searchGetInput.value;
             console.log(searchValue);
-            model.getTracks(searchValue);
+            model.getTracks(searchValue).then(view.printCurrentResults);
         }
 };
 
